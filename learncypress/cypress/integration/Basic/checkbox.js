aa7efbeef65a88e85c981b328a6af63297864b53
Rashidul Hasan <rhratul12@gmail.com>
2021-10-11 00:48:49 +0600

    Update checkbox.js

--- learncypress/cypress/integration/Basic/checkbox.js
+++ learncypress/cypress/integration/Basic/checkbox.js
@@ -1,7 +1,10 @@
+
 describe('Checkbox and Dropdown', function () {
 
     it(' checkbox ', function () {
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+        cy.get('#checkBoxOption1').check().should('be.checked').and('have.value','option1');
+        cy.get('#checkBoxOption1').uncheck().should('not.be.checked');
 
     });
 
