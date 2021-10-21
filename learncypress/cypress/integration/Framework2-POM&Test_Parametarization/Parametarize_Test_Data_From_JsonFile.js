8f7e5c6cce4def08342f79acd8478f40cb63a862
Rashidul Hasan <rhratul12@gmail.com>
2021-10-22 03:30:07 +0600

    Update Parametarize_Test_Data_From_JsonFile.js

--- learncypress/cypress/integration/Framework2-POM&Test_Parametarization/Parametarize_Test_Data_From_JsonFile.js
+++ learncypress/cypress/integration/Framework2-POM&Test_Parametarization/Parametarize_Test_Data_From_JsonFile.js
@@ -2,36 +2,23 @@ describe('Paramaterize Test Data', function () {
 
     before(()=>{
         cy.fixture('example').then(function (data) {
-
-            this.name = data.name;
-            this.gender = data.gender;
             this.product=data.productName;
         })
 
     })
 
-    it('should Open URL', function () {
-        cy.visit('https://rahulshettyacademy.com/angularpractice/');
-    });
 
-    it("Select Multiple Product From JSON FIle", function () {
-        // Click on Shop form Nav
+    it('Select Multiple Product from JSON File', function () {
+
+        cy.visit('https://rahulshettyacademy.com/angularpractice/');
         cy.get(':nth-child(2) > .nav-link').click();
         cy.wait(2000);
 
         this.product.forEach((ele)=>{
-            cy.SelectProduct(ele);
+            cy.log(ele)
+            cy.SelectProduct(ele)
         })
-        
     });
-
-
-    // it('check Data Pass form JSON File', function () {
-    //     cy.log( typeof this.product);
-    //     cy.log( this.name);
-    // });
-
-
 
 
 });
