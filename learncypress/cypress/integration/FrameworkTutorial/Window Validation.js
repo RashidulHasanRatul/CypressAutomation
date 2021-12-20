94bb9255915baf3d3b3f2ee74f917ff605aa0523
Rashidul Hasan <rhratul12@gmail.com>
2021-12-21 02:12:42 +0600

    add Variable and Cypress command

--- learncypress/cypress/integration/FrameworkTutorial/Window Validation.js
+++ learncypress/cypress/integration/FrameworkTutorial/Window Validation.js
@@ -5,8 +5,10 @@ describe("Test contact us form via Automation Test Score",()=>{
     it("Should be able to submit a successful submission via contact us form",()=>{
 
         cy.visit("https://automationteststore.com/index.php?rt=content/contact");
+      
         cy.document().should('have.property',"charset").and('eq',"UTF-8");
         cy.title().should('include','Contact Us')
+        cy.log("Hello World");
         cy.url().should('include','index.php?rt=content/contact')
     })
 })
